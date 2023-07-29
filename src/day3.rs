8b2cfe5c593e0dba74ce5dd577d651cefa6816f4
Tim Peters <tim@catminion.net>
2023-07-29 14:54:54 +0930

    Add day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -6,7 +6,7 @@ pub fn run(input: &str, part: Part) -> String {
         "{}",
         match part {
             Part::One => input.iter().filter(|triangle| possible(triangle)).count(),
-            Part::Two => 0,
+            Part::Two => count_verticle(&input),
         }
     )
 }
@@ -15,6 +15,19 @@ fn possible(triangle: &[u32]) -> bool {
     triangle[0] + triangle[1] > triangle[2]
         && triangle[0] + triangle[2] > triangle[1]
         && triangle[1] + triangle[2] > triangle[0]
+}
+
+fn count_verticle(input: &[Vec<u32>]) -> usize {
+    let mut count = 0;
+    for i in 0..input.len() / 3 {
+        for j in 0..3 {
+            let triangle = [input[i * 3][j], input[i * 3 + 1][j], input[i * 3 + 2][j]];
+            if possible(&triangle) {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u32>> {
